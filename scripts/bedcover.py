1d51dd11ad00e7558e362047c8181dc507ef4bf0
Martin Szyska <martinszyska37@gmail.com>
2020-11-30 13:17:22 +0100

    minor

--- scripts/bedcover.py
+++ scripts/bedcover.py
@@ -5,16 +5,21 @@ import os
 
 def main(s):
 
-    workdir = s.config['workdir']
+    workdir = s.config["workdir"]
     input = s.input
     output = os.path.join(workdir, str(s.output))
 
     params = s.params
     w = s.wildcards
-    sample_name = f"{w.sample}{w.type}"
-
-    get_cover_svg(input.sample, output, sample=sample_name,
-                  exon_cover=params.exon_cover, refgen=params.refgen, log=s.log, prettifyBed=params.prettifyBed)
+    get_cover_svg(
+        input.sample,
+        output,
+        sample=w.sample,
+        exon_cover=params.exon_cover,
+        refgen=params.refgen,
+        log=s.log,
+        prettifyBed=params.prettifyBed,
+    )
 
 
 if __name__ == "__main__":
